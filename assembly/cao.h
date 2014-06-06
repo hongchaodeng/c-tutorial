54ba7f55bbf7bba4a13733fdbd3a63c8e3e2be4e
Hongchao Deng <fengjingchao@gmail.com>
2014-06-06 12:25:45 -0400

    add assembly task

---
+++ assembly/cao.h
@@ -0,0 +1,6 @@
+#ifndef _CAO_H
+#define _CAO_H
+
+void cao();
+
+#endif // for #ifndef _CAO_H
---
+++ assembly/gao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "cao.h"
+
+int main(int argc, char *argv[])
+{
+    cao();
+    return 0;
+}
