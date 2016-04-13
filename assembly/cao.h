a5c3bdcf04471c7b77b0ba2ec5729e73ca189ed9
Hongchao Deng <hongchaodeng1@gmail.com>
2016-04-13 14:07:53 -0700

    rm garbage

--- assembly/cao.h
+++
@@ -1,6 +0,0 @@
-#ifndef _CAO_H
-#define _CAO_H
-
-void cao();
-
-#endif // for #ifndef _CAO_H
--- assembly/gao.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#include "cao.h"
-
-int main(int argc, char *argv[])
-{
-    cao();
-    return 0;
-}
